d6cc5574a91fb7e7ff4c72264880e2c88f67df32
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2024-01-09 10:26:00 +0700

    lab3-3

---
+++ lab3/3/script.js
@@ -0,0 +1,13 @@
+function viewChart() {
+    const rec1 = document.getElementsByClassName('rec1')[0];
+    const rec2 = document.getElementsByClassName('rec2')[0];
+    const rec3 = document.getElementsByClassName('rec3')[0];
+    const rec4 = document.getElementsByClassName('rec4')[0];
+    const rec5 = document.getElementsByClassName('rec5')[0];
+
+    rec1.style.animationPlayState = 'running';
+    rec2.style.animationPlayState = 'running';
+    rec3.style.animationPlayState = 'running';
+    rec4.style.animationPlayState = 'running';
+    rec5.style.animationPlayState = 'running';
+}
