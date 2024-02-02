d223018a445bbe49a16520a508958783e7de4f7d
Sweet eyes <65070047@kmitl.ac.th>
2024-02-02 17:22:43 +0700

    7-1 finish

--- lab7/1/index.php
+++ lab7/1/index.php
@@ -5,13 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>1</title>
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sriracha&display=swap');
+        * {
+            font-family: "Sriracha", cursive;
+        }
+        td {
+            font-size: 20px;
+            padding: 10px;
+            width: 50px;
+            border-bottom: 2px solid black;
+            text-align: center;
+        }
+        h2 {
+            margin-left: 80px;
+        }
+        input {
+            border-radius: 10px;
+        }
+        #submit {
+            transform: scale(1);
+            transition: transform 0.05s ease-in-out;
+        }
+        #submit:hover {
+            transform: scale(1.1);
+        }
 
+        #submit:active {
+            transform: scale(0.9);
+        }
     </style>
 </head>
 <body>
 
     <form action="" method="post">
-        <label for="multiple">กรอกสูตรคูณ</label>
+        <label for="multiple">กรอกสูตรคูณ : </label>
         <input type="text" name="multiple" id="multiple" value="">
         <input type="submit" id="submit" name="submit" value="แสดงตาราง">
     </form>
@@ -20,11 +47,17 @@
     if (isset($_POST["multiple"])) {
         $number = $_POST["multiple"];
         $v = intval($number);
+        echo "<h2>ตารางสูตรคูณแม่ $v</h2>";
+        echo "<table>";
         for ($i = 1; $i <= 12; $i++) {
-            echo $v . " x " . $i . " = ". $v*$i . "<br>";
-        } 
+            $ans = $v * $i;
+            echo "<tr>";
+            echo "<td>$v</td> <td>x</td> <td>$i</td> <td>=</td> <td>$ans</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
     } else {
-        echo "กรอกข้อมูลด้วย...";
+        echo "";
     }
     ?>
 </body>
